8e2758a12567280d2d9aa185c48df610fd4ddaed
Edoardo Prezioso <edward.san.dev@gmail.com>
2017-10-29 13:02:01 +0100

    - Fixed: ACS function SetSkyScrollSpeed didn't work online (addresses 3318).

--- src/cl_main.cpp
+++ src/cl_main.cpp
@@ -7115,6 +7115,16 @@ void ServerCommands::SetMapSky::Execute()
 
 //*****************************************************************************
 //
+void ServerCommands::SetMapSkyScrollSpeed::Execute()
+{
+	if ( isSky1 )
+		level.skyspeed1 = value;
+	else
+		level.skyspeed2 = value;
+}
+
+//*****************************************************************************
+//
 static void client_GiveInventory( BYTESTREAM_s *pByteStream )
 {
 	const PClass	*pType;
